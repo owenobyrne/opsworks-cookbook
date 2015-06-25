9fa1b43e8f04cffad36dfa0048240855828c60f4
Owen O Byrne <owen.obyrne@iol.ie>
2015-06-25 19:19:12 +0100

    Update default.rb

--- opsworks-npm-init/recipes/default.rb
+++ opsworks-npm-init/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'deploy'
 
 bash "npm_install" do
-  cwd "#{node[:deploy]['fire_registration'][:deploy_to]}/src/main/html"
+  cwd "#{node[:deploy]['fire_registration'][:deploy_to]}/current/src/main/html"
   code <<-EOH
     npm install
   EOH
